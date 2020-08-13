29b2faf06ccea3ef98ab3b4305149a52d97c779f
Sam Heutmaker <samheutmaker@gmail.com>
2020-08-13 10:34:37 -0600

    update env vars

--- packages/web/src/env.ts
+++ packages/web/src/env.ts
@@ -1,4 +1,6 @@
-// export const API_URL = 'https://graphql.matchstik.cool/graphql';
-// export const API_URL = 'http://localhost:4000';
-export const API_URL = "http://localhost:3001";
-// export const API_URL = 'http://192.168.1.102:3001';
+const env =
+  (window as any).SERVER_DATA && (window as any).SERVER_DATA.env ? (window as any).SERVER_DATA.env : {};
+export const NODE_ENV = env.NODE_ENV || "development";
+export const DEBUG_ENABLED = env.DEBUG_ENABLED || 1;
+export const API_URL = env.API_URL || "http://localhost:3001";
+export const SENTRY_DSN = env.SENTRY_DSN || null;
